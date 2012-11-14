e7518644cfb1fc63d18370fe8c4968521eec4215
mizutachika <o_hara1145007@mail.goo.ne.jp>
2012-11-14 15:16:31 +0900

    プリント通知
    変更

--- Notice.php
+++ Notice.php
@@ -9,10 +9,10 @@
 	$param = array(
 			'host'=>'ssl://smtp.gmail.com',//メールサーバー
 			'port'=> 465 , //これはSMTPAuthの例。認証が必要ないなら　25　でＯＫ。
-			'from'=>'from@example.com',　//　Return-path: になります。
+			'from'=>'joho.pcp@gmail.com',　//　Return-path: になります。
 			'protocol'=>'SMTP_AUTH',// 認証が必要ないなら、'SMTP'
-			'user'=>'user@gmail.com', //SMTPサーバーのユーザーID
-			'pass' => 'gmail_password', //SMTPサーバーの認証パスワード
+			'user'=>'joho.pcp', //SMTPサーバーのユーザーID
+			'pass' => 'joho_pcp', //SMTPサーバーの認証パスワード
 	);
 	$mail -> smtpServer($param);
 
@@ -20,7 +20,7 @@
 
 	$mail ->to('o_hara1145007@mail.goo.ne.jp','スポック宛');
 	$mail ->subject('メール通知のテスト');
-	$mail ->from('o-hara1045104@mail.goo.ne.jp');
+	$mail ->from('joho.pcp@gmail.com');
 	$mail ->text($message);
 	$return_flag = $mail ->send();
 
